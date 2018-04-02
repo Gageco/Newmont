68d661e741278726a8f3f0d964951d000319e59c
Gage Coprivnicar <gagecoprivnicar@MacBook-Pro-5.local>
2018-04-02 17:24:07 -0600

    improved the graphing api

--- testPrograms/graphAPI.js
+++ testPrograms/graphAPI.js
@@ -12,12 +12,27 @@ request({
   body: jsonPost,
 })
 
-for (i=0; i<16; i++) {
+for (i=0; i<21; i++) {
   jsonPost["data"] = i*i
+  APIrequest()
+  sleep(1000)
+
+
+}
+
+function APIrequest() {
   request({
     url: "http://localhost:8080/data",
     json: true,
     method: "POST",
     body: jsonPost,
   })
+}
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
 }
